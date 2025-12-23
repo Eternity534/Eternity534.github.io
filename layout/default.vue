b8c4ed39994947e949732e0e46b2310f39dbc3f8
minotte <minotte@et.esiea.fr>
2025-12-23 12:44:27 +0100

    feat(global): ajout init page, default vue, app et config

---
+++ layout/default.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <header>
+            <nav>
+                <NuxtLink to="/">Acceuil</NuxtLink>
+                <NuxtLink to="/projets">Mes Projets</NuxtLink>
+                <NuxtLink to="/experience">Mes experiences professionnelle</NuxtLink>
+                <NuxtLink to="/parcours">Mon parcours académique</NuxtLink>
+                <NuxtLink to="/competences">Mes compétences</NuxtLink>
+                <NuxtLink to="/contacts">Contacts</NuxtLink>
+            </nav>
+        </header>
+
+        <main>
+            <slot />
+        </main>
+
+        <footer>
+            <p>&copy; 2025 - Mon Portfolio</p>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+    nav { padding: 20px; border-bottom: 1px solid #ccc; }
+    main { padding: 20px; min-height: 80vh; }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <h1>Page index</h1>
+    </section>
+</template>
+
+<script setup lang="ts">
+    useHead({
+        title: 'Portfolio de Louis Minotte - Étudiant ingénieur informatique',
+        meta: [
+            { name: 'description', content: 'Découvrez mes projets.' }
+        ]
+    })
+</script>
