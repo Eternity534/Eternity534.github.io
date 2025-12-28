d9014e520d23b83aa6fd672a03d71dd5f7708525
minotte <minotte@et.esiea.fr>
2025-12-28 16:00:23 +0100

    fix(global): fix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,7 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
-  compatibilityDate: '2025-12-23',
-  devtools: { enabled: true },
-
-  css: ['~/assets/css/main.css'],
-
-  // Configuration critique pour GitHub Pages
   app: {
     baseURL: '/',
     buildAssetsDir: 'assets',
   },
-
-  nitro: {
-    preset: 'github-pages'
-  },
-
-  ssr: true
 })
